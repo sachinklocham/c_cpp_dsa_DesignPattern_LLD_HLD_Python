32e5cb66c8d6909bb4efa0b8d7165ec55533f1dc
Sachin Kumar <sachinklocham@gmail.com>
2026-01-23 23:50:55 +0530

    rust code updated!

--- Rust/hello_world/src/main.rs
+++ Rust/hello_world/src/main.rs
@@ -14,4 +14,10 @@ fn main() {
     
     //{} is a placeholder for value to be printed indexed from 0 left to right 
     println!("Fruits in garden: {0} apples and {1} oranges where to get {0} more apples, and {1} more oranges", apples_in_garden, oranges);//apples_in_garden is at 0 index and oranges ar 1 index we can refer them multiple times in string with indexes
+
+   // let fruits = apples_in_garden + oranges; // variable binding, assignment, snakeCase, i32 deduced. if we do not use fruits variable then compiler will give warning warning: unused variable: `fruits` so mark as _ in begginning of variable
+    //println!("Total fruits in garden: {fruits}"); // named argument in format string
+
+    let _fruits = apples_in_garden + oranges; // _ in begginning of variable to supress compiler warning
+    
 }
